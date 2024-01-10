4536df6d8bd0795cb4428040505d61200eea8955
hope <yanghaopeng2020@outlook.com>
2024-01-10 17:01:37 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <!-- html -->
+  <div class="app">
+    <h1>Hello VUE3</h1>
+  </div>
+</template>
+
+<script lang="ts">
+/*JS或TS*/
+export default {
+  name: 'App' //组件名
+}
+</script>
+
+<style>
+/*样式*/
+.app {
+  width: 100%;
+  height: 100%;
+  background-color: #f0f0f0;
+}
+</style>
